a35f98002554342b5c8d1d20366fa9cffb5ad207
anbu-selvan10 <anbuselvan253@gmail.com>
2023-12-13 22:12:44 +0530

    visualizations added

--- src/interface.js
+++ src/interface.js
@@ -39,99 +39,95 @@ Return the modified root node.
 
 const code2 = `
     class Solution {
-        private int maxSum;
-        
-        public int maxSumHelper(TreeNode root) {
-            
-            if (root == null) return 0; 
-            
-            int leftMax = maxSumHelper(root.left);
-            int rightMax = maxSumHelper(root.right);
-
-            int maxRightLeft = Math.max(leftMax, rightMax);
-            int maxOneNodeRoot = Math.max(root.val, (root.val + maxRightLeft));
-            int maxAll = Math.max(maxOneNodeRoot, leftMax + rightMax + root.val);
-            
-            maxSum = Math.max(maxSum, maxAll);
-            
-            return maxOneNodeRoot;
-        }
+        private int maxSum = Integer.MIN_VALUE;
 
         public int maxPathSum(TreeNode root) {
-            maxSum = Integer.MIN_VALUE;
             maxSumHelper(root);
             return maxSum;
+        }
+
+        public int maxSumHelper(TreeNode root){
+            if(root == null) return 0;
+
+            int left = maxSumHelper(root.left);
+            int right = maxSumHelper(root.right);
+            maxSum = Math.max(root.val+left+right, maxSum);
+            return Math.max(left,right) + root.val;
         }
     }
 `;
 
 const algo2 = `
-Initialize Maximum Sum Variable: Declare a private variable maxSum to keep track of the maximum path sum. Initialize it to Integer.MIN_VALUE.
+Initialization: maxSum is initialized to Integer.MIN_VALUE to keep track of the maximum path sum across the entire tree.
 
-Define Recursive Helper Function: maxSumHelper - The function takes a TreeNode as an argument and returns the maximum sum of a path starting from the current node to one of its descendants.
+maxPathSum Method: The maxPathSum method is the main entry point that calls the maxSumHelper method and returns the final result.
 
-Base Case: If the current node (root) is null, return 0 (indicating an empty path).
+maxSumHelper Method:
+    Base Case: If the current node (root) is null, return 0.
 
-Recursive Calls: Recursively call maxSumHelper on the left and right subtrees to get the maximum path sums (leftMax and rightMax) starting from their respective child nodes.
+    Recursive Calls: Recursively call maxSumHelper on the left and right subtrees to compute the maximum path sum for each subtree.
 
-Calculate Maximum Path Sums:
-    Calculate the maximum of the left and right path sums (maxRightLeft).
-    Calculate the maximum sum considering only the current node or including one of its child paths (maxOneNodeRoot).
-    Calculate the maximum sum considering all three possibilities: left path, right path, and the current node (maxAll).
+    Update Maximum Path Sum:
+        Calculate the maximum path sum that includes the current node by adding the value of the current node, the maximum path sum from the left subtree, and the maximum path sum from the right subtree.
 
-Update Maximum Sum: Update the global maxSum variable with the maximum of the current maxSum and the calculated maxAll.
+        Update the global maxSum with the maximum value computed in the previous step.
 
-Return Maximum Path Sum for Current Node: Return the maximum sum considering only the current node or including one of its child paths (maxOneNodeRoot). This value will be used by the parent node to calculate its maximum path sum.
-
-Define Main Function: maxPathSum
-    Initialize the maxSum variable to Integer.MIN_VALUE.
-    Call the maxSumHelper function with the root of the binary tree to compute the maximum path sum.
-    Return the final value of maxSum.
+        Return the maximum of the sum of the current node and the maximum path sum from either the left or right subtree. This value represents the maximum path sum for the current subtree.
 `;
 
 const code3 = `
     class Solution {
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-            if(root == null || root == p || root == q){
+            if (root.val > p.val && root.val > q.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            } else if (root.val < p.val && root.val < q.val) {
+                return lowestCommonAncestor(root.right, p, q);
+            } else {
                 return root;
             }
-
-            TreeNode left = lowestCommonAncestor(root.left, p, q);
-            TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-            if(left != null && right != null){
-                return root;
-            }
-
-            return (left!=null ? left : right);
         }
-}
+    }
 `;
 
 const algo3 = `
-Base Case:
-    If the current node (root) is null or equal to either of the two nodes (p or q), return the current node.
-    If root is null, it indicates that the end of a branch is reached, and no common ancestor is found.
-    If root is equal to p or q, it means that one of the nodes is found, and the current node is a potential common ancestor.
+Base Case: Checks if the current node is null or equal to either of the two nodes (p or q).
 
-Recursive Calls:
-    Recursively call lowestCommonAncestor on the left and right subtrees to search for nodes p and q.
-    Store the results in variables left and right.
+Recursive Calls: Determines whether to traverse the left or right subtree based on the values of p, q, and the current root.
 
-Check Results:
-    If both left and right are not null, it means that nodes p and q are found in different subtrees, and the current node (root) is the lowest common ancestor.
-    Return the current node as the LCA.
-
-Return Non-Null Result:
-    If either left or right is non-null, return the non-null result.
-    If both left and right are null, return null.
+Found Lowest Common Ancestor: If the conditions are not met for recursive calls, it means one node is in the left subtree, and the other is in the right subtree. The current root is the lowest common ancestor, and it is returned.
 `;
 
 const CodeAlgorithmCard = () => {
-    const [isButtonClicked, setButtonClicked] = useState(false);
+    const [isButtonClicked1, setButtonClicked1] = useState(false);
+    const [isButtonClicked2, setButtonClicked2] = useState(false);
+    const [isButtonClicked3, setButtonClicked3] = useState(false);
 
-    const handleButtonClick = () => {
-    setButtonClicked(true);
+    const [isButtonClicked01, setButtonClicked01] = useState(false);
+    const [isButtonClicked02, setButtonClicked02] = useState(false);
+    const [isButtonClicked03, setButtonClicked03] = useState(false);
+
+    const handleButtonClick1 = () => {
+        setButtonClicked1(true);
+    };
+
+    const handleButtonClick2 = () => {
+        setButtonClicked2(true);
+    };
+
+    const handleButtonClick3 = () => {
+        setButtonClicked3(true);
+    };
+
+    const handleButtonClick01 = () => {
+        setButtonClicked01(true);
+    };
+
+    const handleButtonClick02 = () => {
+        setButtonClicked02(true);
+    };
+
+    const handleButtonClick03 = () => {
+        setButtonClicked03(true);
     };
 
   return (
@@ -152,16 +148,27 @@ const CodeAlgorithmCard = () => {
             </div>
             <img src={require('./images/gst.png')} className='code-image'/>
             <br />
-            {isButtonClicked ? (
+            {isButtonClicked1 ? (
             <video width="640" height="360" controls>
                 <source src={require('./videos/gst_vid.mp4')} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             ) : (
-            <button type="submit" onClick={handleButtonClick}>
+            <button type="submit" onClick={handleButtonClick1}>
                 Visualization with explanation
             </button>
             )}
+            <hr />   
+            {isButtonClicked01 ? (
+                <div>
+                <p>TC: The time complexity of this code is O(N), where N is the number of nodes in the binary search tree (BST). The reason for this is that the code visits each node exactly once, and the work done at each node is constant time.</p>
+                <p>SC: The space complexity is O(H), where H is the height of the binary search tree. In the worst case, where the tree is skewed and has a height equal to the number of nodes (N), the space complexity would be O(N). However, in a balanced BST, the height is logarithmic in the number of nodes (H = log(N)), so the space complexity becomes O(log(N)).</p>
+                </div>
+                ) : (
+                <button type="submit" onClick={handleButtonClick01}>
+                    Time and Space Complexity Analysis
+                </button>
+                )}
         </div>
         <br />
         <div>
@@ -178,7 +185,17 @@ const CodeAlgorithmCard = () => {
             </div>
             <img src={require('./images/mps.png')} className='code-image'/>
             <br />
-            <button type='submit'>Visualization with explanation</button>
+            {isButtonClicked2 ? (
+                <video width="640" height="360" controls>
+                    <source src={require('./videos/mps_vid.mp4')} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                ) : (
+                <button type="submit" onClick={handleButtonClick2}>
+                    Visualization with explanation
+                </button>
+                )}
+            
         </div>
         <br />
         <div>
@@ -195,7 +212,16 @@ const CodeAlgorithmCard = () => {
             </div>
             <img src={require('./images/lca.jpg')} className='code-image'/>
             <br />
-            <button type='submit'>Visualization with explanation</button>
+            {isButtonClicked3 ? (
+            <video width="640" height="360" controls>
+                <source src={require('./videos/lca_vid.mp4')} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            ) : (
+            <button type="submit" onClick={handleButtonClick3}>
+                Visualization with explanation
+            </button>
+            )}
         </div>
     </div>
   );
